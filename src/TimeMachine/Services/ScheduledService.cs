bf477f0ea6a54138adfb227233323ec050c47fb1
Ricky Limka <rikki@chikurin.example.org>
2019-02-28 17:32:53 +0900

    Populate database every hour (xx:00) using external resource.

--- src/TimeMachine/Services/ScheduledService.cs
+++ src/TimeMachine/Services/ScheduledService.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TimeMachine.DAL;
+using TimeMachine.Models;
 
 namespace TimeMachine.Services
 {
@@ -33,23 +34,29 @@ namespace TimeMachine.Services
                 || (lastHour == 23 && DateTime.Now.Hour == 0))
                 {
                     lastHour = DateTime.Now.Hour;
-
-                    // Create and run a scoped task with dbContext.
-                    using (var scope = _srvFactory.CreateScope())
-                    {
-                        var db = scope.ServiceProvider.GetRequiredService<TimeMachineContext>();
-                        RunTask(db);
-                    }
-                    
+                    RunTask();
                 }
             });
 
             timer.Enabled = true;
         }
 
-        public void RunTask(TimeMachineContext db)
+        public async void RunTask()
         {
-            // TODO: Convert to async. Save to database here.
+            var res           = await _http.GetAsync(datasource);
+            var subredditJson = await res.Content.ReadAsStringAsync();
+            
+            // Create and run a scoped task with dbContext.
+            using (var scope = _srvFactory.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<TimeMachineContext>();
+                db.Snapshots.Add(new SubRedditSnapshot
+                {
+                    Timestamp = DateTime.Now,
+                    Json = subredditJson
+                });
+                db.SaveChanges();
+            }
         }
 
     }
