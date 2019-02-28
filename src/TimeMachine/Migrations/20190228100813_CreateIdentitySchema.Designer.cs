0abfcfa9cfee46c1f36b2ee44f5ea37c71f6c4c1
Ricky Limka <rikki@chikurin.example.org>
2019-02-28 19:12:38 +0900

    .NET Core Identity table schema Migration.

--- src/TimeMachine/Migrations/20190228100813_CreateIdentitySchema.Designer.cs
+++ src/TimeMachine/Migrations/20190228100813_CreateIdentitySchema.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TimeMachine.DAL;
 
 namespace TimeMachine.Migrations
 {
     [DbContext(typeof(TimeMachineContext))]
-    partial class TimeMachineContextModelSnapshot : ModelSnapshot
+    [Migration("20190228100813_CreateIdentitySchema")]
+    partial class CreateIdentitySchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
